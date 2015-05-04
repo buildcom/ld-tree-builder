b337354781d0eb9e578df474fa475c07f1e4af1b
Seth Davis <sethd17@gmail.com>
2015-05-04 16:48:34 -0500

    Second commit

---
+++ examples/js/app.js
@@ -0,0 +1,29 @@
+(function () {
+  'use strict';
+
+  angular.module('demoApp', ['ui.tree', 'ngRoute'])
+
+    .config(['$routeProvider', function ($routeProvider) {
+      $routeProvider
+        .when('/', {
+          controller: 'BasicExampleCtrl',
+          templateUrl: 'views/basic-example.html'
+        })
+        .when('/basic-example', {
+          controller: 'BasicExampleCtrl',
+          templateUrl: 'views/basic-example.html'
+        })
+        .when('/connected-trees', {
+          controller: 'ConnectedTreesCtrl',
+          templateUrl: 'views/connected-trees.html'
+        })
+        .when('/filter-nodes', {
+          controller: 'FilterNodesCtrl',
+          templateUrl: 'views/filter-nodes.html'
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
+    }])
+
+})();
